b22ce868a10609edb6c0acc3ce703330a164e25a
Cannon Palms <cannonpalms@me.com>
2019-09-10 01:53:21 -0400

    bump version to v1.1.1

--- FasterLooting.lua
+++ FasterLooting.lua
@@ -2,20 +2,22 @@
 -- Name: FasterLooting
 -- Author: cannon
 -- Description: Auto loots all items without the delay currently in Classic
--- Version: 1.0.0
+-- Version: 1.1.1
 
 -- Time delay
-local tDelay = 0
+local delay = 0
+local DEBOUNCE_INTERVAL = 0.3
+
 
 -- Fast loot function
-local function FastLoot()
-    if GetTime() - tDelay >= 0.3 then
-        tDelay = GetTime()
+function FastLoot()
+    if GetTime() - delay >= DEBOUNCE_INTERVAL then
+        delay = GetTime()
         if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
             for i = GetNumLootItems(), 1, -1 do
                 LootSlot(i)
             end
-            tDelay = GetTime()
+            delay = GetTime()
         end
     end
 end
