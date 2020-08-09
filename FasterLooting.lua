06bcfed934fca6cb1495ea7e89b3b6c58b465114
Lauri Junkkari <latenssi@gmail.com>
2020-08-09 23:26:16 +0300

    Even faster looting, update interface version

--- FasterLooting.lua
+++ FasterLooting.lua
@@ -1,28 +1,44 @@
 -- File: FasterLooting.lua
 -- Name: FasterLooting
--- Author: cannon
+-- Author: cannon, latenssi
 -- Description: Auto loots all items without the delay currently in Classic
--- Version: 1.1.1
+-- Version: 1.2.0
 
--- Time delay
-local delay = 0
 local DEBOUNCE_INTERVAL = 0.3
 
+local delay = 0
+local shouldFastLoot = false
+local LootFrame_OnEvent_default = LootFrame:GetScript("OnEvent")
 
 -- Fast loot function
 function FastLoot()
     if GetTime() - delay >= DEBOUNCE_INTERVAL then
-        delay = GetTime()
-        if GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE") then
-            for i = GetNumLootItems(), 1, -1 do
-                LootSlot(i)
-            end
-            delay = GetTime()
+        for i = GetNumLootItems(), 1, -1 do
+            LootSlot(i)
         end
+        delay = GetTime()
     end
 end
 
--- event frame
-local faster = CreateFrame("Frame")
-faster:RegisterEvent("LOOT_READY")
-faster:SetScript("OnEvent", FastLoot)
+function LootFrame_OnEvent_modified(...)
+    local _, event = ...
+
+    if event == "LOOT_READY" then
+        shouldFastLoot = GetCVarBool("autoLootDefault") ~= IsModifiedClick("AUTOLOOTTOGGLE")
+    elseif event == "LOOT_CLOSED" then
+        -- Reset
+        shouldFastLoot = false
+    end
+
+    if event == "LOOT_READY" and shouldFastLoot then
+        -- Run our FastLoot handler
+        -- Note: Ignoring all LootFrame registered events to prevent displaying the loot window etc.
+        -- Note: This is run on LOOT_READY so we do not need to wait for the LOOT_OPENED event
+        FastLoot()
+    elseif not shouldFastLoot then
+        -- Run the default handler
+        LootFrame_OnEvent_default(...)
+    end
+end
+
+LootFrame:SetScript("OnEvent", LootFrame_OnEvent_modified)
